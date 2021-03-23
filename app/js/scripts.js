4466e0ce7ca9844a3db8d8014355795004cbfcf9
Anastasia <18anastasiasneshko@gmail.com>
2021-03-23 13:25:52 +0200

    added training 13

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,10 +1,46 @@
 let i = 0;
-console.log(typeof(i));
+console.log(typeof (i));
+
+if(i === 0) {
+  console.log(true)
+}
+else {
+  console.log(false)
+}
+console.log((i == 0));
+
+// console.log((i == 0) ? true : false);
 
 
-if(i === '0') {
-     console.log(true);
-}
-else{
-    console.log(false);
-}
+/*PRACTIC*/
+
+// let a = 6; b = 3;
+// console.log(a > b ? true : false);
+
+
+// let a = 3; b = 3;
+// console.log(a == b ? true : false);
+
+
+// let a = 3; b = 6;
+// console.log(a != b ? true : false);
+
+
+// let a = 1; b = "";
+// console.log(a === b ? true : false);
+
+
+// console.log(('B' > 'A') ? true : false);
+
+
+// console.log(('a' < 'Z') ? true : false);
+
+
+// let a = false; b = 0;
+// console.log(a >= b ? true : false);
+
+
+// console.log(('2'===2) ? 'ok' : 'not');
+
+let str = prompt("Input your digit", 0);
+alert (str);
